a438f2aca4d5bbf2534b851871e29e6f709d666e
Flashcv <40182253+Flashcv@users.noreply.github.com>
2018-10-04 14:23:20 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,151 +1,66 @@
-$(document).ready(function () {
-    var out = '';
-    var uniqYears = [];
-    var ActualDate = new Date();
-    document.get
-    document.getElementById("check").onclick = function () {
-        let login = document.getElementById("login");
-        let password = document.getElementById("password");
-        //let top = document.getElementsByClassName("top");
-        let check = document.getElementById("check");
-        let logout = document.getElementById("logout");
-        let logged = document.getElementById("logged");
-        let labellogin = document.getElementById("labellogin");
-        let labelpassword = document.getElementById("labelpassword");
-        //let disabled = document.getElementsByClass("disabled");
-        // for (let i = 0; i < login.length; i++) {
-            if (login.value == "" && password.value == "") {
-                password.style.display="none";
-                login.style.display="none";
-                check.style.display = "none";
-                logout.style.display = "block";
-                logged.style.display = "block";
-                $("#labellogin").toggleClass("hidden");
-                $("#labelpassword").toggleClass("hidden");
-                $(".disabled").toggleClass("disabled");
-                
-                
-                // login.style.display = "none";
-                // password.style.display = "none";
-                //top[i].style.display = "none";
-            }
-            else {
-                alert("incorrect login or password");
-                login.value = "";
-                password.value = "";
-            }
-        
+let data = [];
+var config = {
+    apiKey: "AIzaSyCY35X_IC6tnFgrRGrWmWbl0fcj6039zHc",
+    authDomain: "demo1-41cbe.firebaseapp.com",
+    databaseURL: "https://demo1-41cbe.firebaseio.com",
+    projectId: "demo1-41cbe",
+    storageBucket: "demo1-41cbe.appspot.com",
+    messagingSenderId: "427473126991"
+};
+firebase.initializeApp(config);
+
+var out = '';
+var uniqYears = [];
+var ActualDate = new Date();
+const months = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec'
+]
+const days = [
+        'Sunday',
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday'
+    ]
+    //to delete duplicate years
+function unique(arr) {
+    var obj = {}
+    for (var i = 0; i < arr.length; i++) {
+        var str = arr[i];
+        obj[str] = true;
     }
-    document.getElementById("logout").onclick = function () {
-        let login = document.getElementById("login");
-        let password = document.getElementById("password");
-        //let top = document.getElementsByClassName("top");
-        let check = document.getElementById("check");
-        let logout = document.getElementById("logout");
-        let logged = document.getElementById("logged");
+    return Object.keys(obj);
+}
+//to render event front/back card + modal
+function renderEvent(eventData) {
+    let speakerData = '';
+    let modal = '';
 
-        password.style.display = "block";
-        login.style.display = "block";
-        check.style.display = "block";
-        logout.style.display = "none";
-        logged.style.display = "none";
-
-    }
-    // function to delete duplicate years except one ==============================================================
-    function unique(arr) {
-        var obj = {}
-        for (var i = 0; i < arr.length; i++) {
-            var str = arr[i];
-            obj[str] = true;
-        }
-        return Object.keys(obj);
-    }
-    // list of years from JSON file ==============================================================
-    $.getJSON('data.json', function (data) {
-        var allYears = [];
-        let outroDefault = '';
-        let months = [
-            'Jan',
-            'Feb',
-            'Mar',
-            'Apr',
-            'May',
-            'Jun',
-            'Jul',
-            'Aug',
-            'Sep',
-            'Oct',
-            'Nov',
-            'Dec'
-        ]
-        let days = [
-            'Sunday',
-            'Monday',
-            'Tuesday',
-            'Wednesday',
-            'Thursday',
-            'Friday',
-            'Saturday'
-        ]
-        data.forEach(function (element) {
-            let date = new Date(element.date)
-            allYears.push(date.getFullYear())
-            uniqYears = unique(allYears.sort())
-        })
-        for (var i = 0; i < uniqYears.length; i++) {
-            if (uniqYears[i] == "2018") {
-                out += '<li class="year activeYear">' + uniqYears[i] + '</li>'
-            } else {
-                out += '<li class="year">' + uniqYears[i] + '</li>'
-            }
-        }
-
-        $('#slider').append(out);
-
-        // default index.html ==============================================================
-        data.forEach(function (element) {
-            let date = new Date(element.date)
-            if (date.getFullYear() == ActualDate.getFullYear()) {
-                let speakerData = '';
-                outroDefault += `
-                <div class="container">
-                    <div class="card">
-                        <div class="front">
-                            <div class="contentfront">
-                            <div class="month">
-                            <span class="eventName">
-                            ${element.name}
-                            </span>
-                            <div class="eventPlace">
-                            ${element.address}
-                            </div>
-                            </div>
-                            <div class="date">
-                                <div class="datecont">
-                                <div id="date">${date.getDate()}</div>
-                                <div id="day">${days[date.getDay()]}</div>
-                                <div id="month">${months[date.getMonth()] + ' / ' + date.getFullYear()}</div>
-                                </div>
-                            </div>
-                            </div>
-                        </div>
-                        <div class="back">
-                            ${speakerData}
-                        </div>
-                    </div>
-                </div>`
-            }
-            $('#name').html(outroDefault);
-            // $('.year').removeClass('year').addClass('active');
-        })
-        $('.year').click(function () {
-            $('.year').removeClass('activeYear');
-            $(this).toggleClass('activeYear');
-            document.getElementById('name').innerHTML = ''
-            $('#name').html('');
-            let outro = '';
-            let modal = '';
-            let thead = `
+    eventData.speaker.forEach(function(speaker, index, data) {
+        modal += `
+        <div id="${speaker.firstName + speaker.lastName}" class="modal">
+            <span>Speaker:</span> 
+            <span class="titleClass">${speaker.firstName + ' ' + speaker.lastName}</span>
+            <p>from:    ${speaker.city}, ${speaker.country} </p>
+            <p> Description </p>
+            <p> ${speaker.description} </p>
+            <a href="#" rel="modal:close">Close</a>
+        </div>`
+        if (index === 0) {
+            speakerData += `
             <table class="blueTable">
                 <thead>
                     <tr class="rowClass">
@@ -154,81 +69,219 @@ $(document).ready(function () {
                         <th>Speaker</th>
                     </tr>
                 </thead>
-            <tbody>
-            `
-            let tfoot = `
-            </tbody>
-            </table>
-            `
-            yearOnClick = $(this).text()
-            data.forEach(element => {
-                let date = new Date(element.date)
-                if (parseInt(yearOnClick) == date.getFullYear()) {
-                    let speakerData = '';
+            <tbody>`
+        }
 
-                    element.speaker.forEach(each => {
-                        speakerData += `
-                        <tr>
-                            <td>${each.time}</td>
-                            <td>${each.topic}</td>
-                            <td><a class="buttonClass" href="#${each.firstName + each.lastName}" rel="modal:open">${each.firstName + " " + each.lastName}</a></td>
-                        </tr>
-                        
-                        `
-                        modal += `
-                        <div id="${each.firstName + each.lastName}" class="modal">
-                        <p>Speaker: ${each.firstName + ' ' + each.lastName}, from ${each.city}, ${each.country} </p>
-                        <p> ${each.description} </p>
-                        <a href="#" rel="modal:close">Close</a>
-                        </div>
-                        `
-                        // </div>
+        speakerData += `
+            <tr>
+                <td>${speaker.time}</td>
+                <td>${speaker.topic}</td>
+                <td><a class="buttonClass" href="#${speaker.firstName + speaker.lastName}" rel="modal:open">${speaker.firstName + " " + speaker.lastName}</a></td>
+            </tr>`
 
-                    });
+        if (index === (data.length - 1)) {
+            speakerData += `
+                </tbody>
+                </table>
+                `
+        }
 
-                    outro += `
-                        <div class="container">
-                            <div class="card">
-                                <div class="front">
-                                    <div class="contentfront">
-                                    <div class="month">
-                                    <span class="eventName">
-                                    ${element.name}
-                                    </span>
+    });
+    const date = new Date(eventData.date);
+    $('#modal').append(modal);
+    return `  
+            <div class="container">
+                <div class="card">
+                    <div class="front">
+                        <div class="contentfront">
+                            <div class="month">
+                                <span class="eventName">
+                                    ${eventData.name}
+                                </span>
                                     <div class="eventPlace">
-                                    ${element.address}
+                                        ${eventData.address}
                                     </div>
-                                    </div>
-                                    <div class="date">
-                                        <div class="datecont">
-                                        <div id="date">${date.getDate()}</div>
-                                        <div id="day">${days[date.getDay()]}</div>
-                                        <div id="month">${months[date.getMonth()] + ' / ' + date.getFullYear()}</div>
-                                        </div>
-                                    </div>
-                                    </div>
-                                </div>
-                                <div class="back">
-                                    ${thead + speakerData + tfoot}
+                            </div>
+                            <div class="date">
+                                <div class="datecont">
+                                <div id="date">${date.getDate()}</div>
+                                <div id="day">${days[date.getDay()]}</div>
+                                <div id="month">${months[date.getMonth()] + ' / ' + date.getFullYear()}</div>
                                 </div>
                             </div>
-                        </div>`
-                }
-                $('#name').html(outro);
-                $('#modal').html(modal);
-            })
-            // $('.card').click(function() {
-            //     //alert("asdasd");
-            //     $(this).toggleClass('flipped');
-            // })
-
-            $('.card').click(function (e) {
-                if (e.target.className === "buttonClass") return;
-                //alert("asdasd");
-                $(this).toggleClass('flipped');
-            })
-        })
-
+                        </div>
+                    </div>
+                    <div class="back">
+                        ${speakerData}
+                    </div>
+                </div>
+            </div>`
+}
+//years list
+function renderYears(events) {
+    var allYears = [];
+    events.forEach(function(element) {
+        let date = new Date(element.date)
+        allYears.push(date.getFullYear())
+        uniqYears = unique(allYears.sort())
     })
+    for (var i = 0; i < uniqYears.length; i++) {
+        if (uniqYears[i] == "2018") {
+            out += '<li class="year activeYear">' + uniqYears[i] + '</li>'
+        } else {
+            out += '<li class="year">' + uniqYears[i] + '</li>'
+        }
+    }
+    return out;
+}
+//flip card
+function flipCard(e) {
+    if (e.target.className === "buttonClass") return;
+    $(this).toggleClass('flipped');
+}
+//main
+window.onload = (function() {
+    let database = firebase.database();
+    database.ref().on("value", function(snap) {
+        if (snap.exists()) {
+            data = snap.val();
+            let outroDefault = '';
+            $('#slider').html(renderYears(data));
 
+            let currentYearEvent = data.filter(function(event) {
+                    let date = new Date(event.date);
+                    return date.getFullYear() === ActualDate.getFullYear();
+                })
+                //default page (current year)
+            currentYearEvent.forEach(function(event) {
+                $('#name').append(renderEvent(event))
+            })
+            $('.card').click(flipCard);
+
+            $('.year').click(function() {
+                $('.year').removeClass('activeYear');
+                $(this).toggleClass('activeYear');
+                document.getElementById('name').innerHTML = ''
+                $('#name').html('');
+                //onclick year
+                yearOnClick = $(this).text()
+                let filteredData = data.filter(function(event) {
+                    let date = new Date(event.date);
+                    return parseInt(yearOnClick) === date.getFullYear();
+                })
+
+                filteredData.forEach(function(event) {
+                    $('#name').append(renderEvent(event));
+                })
+                $('.card').click(flipCard);
+            })
+
+
+        }
+    });
 });
+
+function formSend(form) {
+
+    let val = {
+        "name": form.EventName1.value,
+        "date": form.EventDate1.value,
+        "city": form.EventCity1.value,
+        "address": form.EventAddress1.value,
+        "speaker": [{
+            "time": form.EventTime1.value,
+            "topic": form.EventTopic1.value,
+            "firstName": form.SpeakerName1.value,
+            "lastName": form.SpeakerLastName1.value,
+            "country": form.SpeakerCountry1.value,
+            "city": form.SpeakerCity1.value,
+            "description": form.SpeakerDescription1.value
+        }]
+    }
+    let ref = firebase.database().ref();
+    let eventsRef = ref.child(data.length);
+    let eventRef = eventsRef.update(val);
+
+    ref.child(eventsRef).update(val);
+
+
+}
+//login button
+document.getElementById("check").onclick = function() {
+        let login = document.getElementById("login");
+        let password = document.getElementById("password");
+        let check = document.getElementById("check");
+        let logout = document.getElementById("logout");
+        let logged = document.getElementById("logged");
+        if (login.value == "admin" && password.value == "admin") {
+            password.style.display = "none";
+            login.style.display = "none";
+            check.style.display = "none";
+            logout.style.display = "block";
+            logged.style.display = "block";
+            $("#labellogin").toggleClass("hidden");
+            $("#labelpassword").toggleClass("hidden");
+            $(".disabled").toggleClass("disabled");
+
+        } else {
+            alert("incorrect login or password");
+            login.value = "";
+            password.value = "";
+        }
+    }
+    //logout button
+document.getElementById("logout").onclick = function() {
+        window.location.replace('index.html');
+    }
+    //add speaker form
+let counter = 0;
+$("#addSpeaker").click(function() {
+    let oneMore = '';
+    if (counter < 4) {
+        counter += 1;
+        oneMore +=
+            `
+            <div class="addFormBody">
+                <fieldset>
+                    <label class="eventName">Add speaker</label>
+                    <div class="field">
+                        <label for="EventTime">Event time</label>
+                        <select id="EventTime${counter}" name="EventTime">
+                    <option value="9:30-10:30" selected>9:30-10:30</option>
+                    <option value="10:30-11:30">10:30-11:30</option>
+                    <option value="11:30-12:30">11:30-12:30</option>
+                    <option value="12:30-13:30">12:30-13:30</option>
+                </select>
+                    </div>
+                    <div class="field">
+                        <label for="Topic">Topic</label>
+                        <input type="text" id="EventTopic${counter}" name="EventTopic">
+                    </div>
+                    <div class="field">
+                        <label for="SpeakerName">First Name</label>
+                        <input type="text" id="SpeakerName${counter}" name="SpeakerName">
+                    </div>
+                    <div class="field">
+                        <label for="SpeakerLastname">Last Name</label>
+                        <input type="text" id="SpeakerLastname${counter}" name="SpeakerLastName">
+                    </div>
+                    <div class="field">
+                        <label for="SpeakerFromCountry">From (Country)</label>
+                        <input type="text" id="SpeakerCountry${counter}" name="SpeakerCountry">
+                    </div>
+                    <div class="field">
+                        <label for="SpeakerFromCity">From (City)</label>
+                        <input type="text" id="SpeakerCity${counter}" name="SpeakerCity">
+                    </div>
+                    <div class="field">
+                        <label for="SpeakerDescription">Description</label>
+                        <input type="text" id="SpeakerDescription${counter}" name="SpeakerDescription">
+                    </div>
+
+            </fieldset>
+        </div>`
+    }
+
+    $('#oneMore').append(oneMore);
+    console.log(counter);
+})
